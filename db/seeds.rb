e0a53760959feb550e1263d12044a733b631dd6b
sarah dakkak <sarah.dakkak1@gmail.com>
2021-02-25 17:20:17 -0500

    fixed cloudinary bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,23 +2,13 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-require 'rest-client'
-require 'open-uri'
+
 
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Cleaning database..."
 Shop.destroy_all
 
-puts "Creating shops..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE"}
-pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ"}
-
-[ dishoom, pizza_east ].each do |attributes|
-  shop = Shop.create!(attributes)
-  puts "Created #{shop.name}"
-end
-puts "Finished!"
+puts "destroy all shops"
 
 
 
