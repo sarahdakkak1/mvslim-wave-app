0212798b16f91170f7900bf40d22e162b25e1ca6
sarah dakkak <sarah.dakkak1@gmail.com>
2021-02-25 15:50:44 -0500

    removed app.js

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -5,12 +5,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import App from '../components/App'
 
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <Hello />,
     document.body.appendChild(document.createElement('div')),
   )
 })
